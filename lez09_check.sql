0496637a9b6f687c3af72ad0863a3f62f0ad3bdc
Giovanni Pace <g.pace@braintechinnovations.it>
2024-05-17 12:28:37 +0200

    Aggiunto SQL

--- lez09_check.sql
+++ lez09_check.sql
@@ -22,9 +22,34 @@ INSERT INTO Prodotto(nome, descrizione, categoria, quantita, prezzo) VALUES
 INSERT INTO Prodotto(nome, descrizione, categoria, quantita, prezzo) VALUES
 ("Lampada", "Prova", "Illuminazione", 1, 23.55);
 
+INSERT INTO Prodotto (nome, descrizione, categoria, quantita, prezzo) 
+VALUES 
+('Trapano Elettrico', 'Trapano elettrico a batteria da 18V', 'Bricolage', 10, 79.99);
+
+INSERT INTO Prodotto (nome, descrizione, categoria, quantita, prezzo) 
+VALUES 
+('Lampada da Tavolo', 'Lampada da tavolo con luce LED regolabile', 'Illuminazione', 25, 49.90);
+
+INSERT INTO Prodotto (nome, descrizione, categoria, quantita, prezzo) 
+VALUES 
+('Set di Cacciaviti', 'Set di cacciaviti professionali con impugnatura ergonomica', 'Bricolage', 15, 25.50);
+
+INSERT INTO Prodotto (nome, descrizione, categoria, quantita, prezzo) 
+VALUES 
+('Lampadario Moderno', 'Lampadario moderno con finitura in metallo', 'Illuminazione', 8, 120.00);
+
+INSERT INTO Prodotto (nome, descrizione, categoria, quantita, prezzo) 
+VALUES 
+('Sega Circolare', 'Sega circolare con lama da 185mm', 'Bricolage', 5, 89.99);
+
+INSERT INTO Prodotto (nome, descrizione, categoria, quantita, prezzo) 
+VALUES 
+('Faretti da Incasso', 'Set di 3 faretti da incasso con luci LED', 'Illuminazione', 20, 35.00);
+
+
 -- QL
 SELECT * FROM Prodotto;
 SELECT nome, descrizione FROM Prodotto;
 
-
 SELECT * FROM Prodotto WHERE categoria = "Bricolage";
+
